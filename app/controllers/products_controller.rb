6c1f67e2dc69b30682531a27a0da9eac292029f4
Haroon Khaliqi <haroonkhaliqi12@Haroons-MBP.attlocal.net>
2023-05-24 11:00:28 -0700

    created carted_products model and removed product_id and quantity from orders model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,12 @@
 class ProductsController < ApplicationController
   before_action :authenticate_admin, except: [:index, :show]
-  
+
   def index
     @products = Product.all
+    if params["category"]
+      category = Category.find_by(name: params["category"])
+      @products = category.products
+    end
     render :index
   end
 
